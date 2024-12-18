72ba31f87a78565c2ab97492723b6b28f18cadf1
Jevi Adriansyah <jeviadriansyah25@gmail.com>
2024-12-18 12:56:35 +0700

    update

--- public/Dashboard.php
+++ public/Dashboard.php
@@ -11,6 +11,8 @@ if (!isset($_SESSION['username'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style1.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
   <body>
@@ -33,7 +35,7 @@ if (!isset($_SESSION['username'])) {
               <li class="nav-item">
                 <a class="nav-link active text-light" aria-current="page" href="#">Laporan</a>
               </li>
-              <li class="nav-item dropdown">
+              <li class="nav-item dropdown dropstart">
                 <a class="nav-link dropdown-toggle active text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Akun
                 </a>
@@ -53,14 +55,95 @@ if (!isset($_SESSION['username'])) {
                     Wellcome To Bandi Car
                 </h1>
                 <h6>Menjadi pilihan utama untuk penyewaan mobil dengan pelayanan pelanggan yang unggul dan armada yang dapat diandalkan di seluruh Indonesia.</h6>
-                <a class="btn btn-lg btn-primary mt-5" href="Peminjaman.html" role="button">Mulai Sewa Mobil</a>
+                <a class="btn btn-lg btn-primary mt-5" href="Peminjaman.php" role="button">Mulai Sewa Mobil</a>
             </div>
             <div class="col-md-6">
                 <img src="11452724.png" width="80%" alt="">
             </div>
         </div>
       </div>
-      <div class="container">
+      
+      <div class="container container-product">
+        <div class="row">
+            <div class="col">
+                <h1 class="text-center mt-3">Produk</h1>
+                <p class="text-center">BandiCar menawarkan produk produk berkualitas yang bisa anda sewa.</p>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col">
+                <div class="row">
+                    <div class="col-sm-4">
+                    <div class="card shadow" style="width: 21rem; height: 25rem; ">
+                      <img src="White-2.png" class="card-img-top" alt="..." style="width: 21rem; height: 25rem;">
+                      <div class="card-body">
+                        <h5 class="card-title">Avanza</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary d-grid gap-2">Detail</a>
+                      </div>
+                    </div>
+                    </div>
+                    <div class="col-sm-4">
+                    <div class="card shadow" style="width: 21rem; height: 25rem;">
+                      <img src="file.png" class="card-img-top" alt="..." style="width: 21rem; height: 25rem;">
+                      <div class="card-body">
+                        <h5 class="card-title">Xenia</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary d-grid gap-2">Detail</a>
+                      </div>
+                    </div>
+                    </div>
+                    <div class="col-sm-4">
+                    <div class="card shadow" style="width: 21rem; height: 25rem;">
+                      <img src="Brio-PNG.png" class="card-img-top" alt="..." style="width: 21rem; height: 25rem;">
+                      <div class="card-body">
+                        <h5 class="card-title">Brio</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary d-grid gap-2">Detail</a>
+                      </div>
+                    </div>
+                      </div>
+                  </div>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col">
+                <div class="row mt-5">
+                    <div class="col-sm-4">
+                    <div class="card shadow" style="width: 21rem; height: 25rem; ">
+                      <img src="White-2.png" class="card-img-top" alt="..." style="width: 21rem; height: 25rem;">
+                      <div class="card-body">
+                        <h5 class="card-title">Avanza</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary d-grid gap-2">Detail</a>
+                      </div>
+                    </div>
+                    </div>
+                    <div class="col-sm-4">
+                    <div class="card shadow" style="width: 21rem; height: 25rem;">
+                      <img src="file.png" class="card-img-top" alt="..." style="width: 21rem; height: 25rem;">
+                      <div class="card-body">
+                        <h5 class="card-title">Xenia</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary d-grid gap-2">Detail</a>
+                      </div>
+                    </div>
+                    </div>
+                    <div class="col-sm-4">
+                    <div class="card shadow" style="width: 21rem; height: 25rem;">
+                      <img src="Brio-PNG.png" class="card-img-top" alt="..." style="width: 21rem; height: 25rem;">
+                      <div class="card-body">
+                        <h5 class="card-title">Brio</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary d-grid gap-2">Detail</a>
+                      </div>
+                    </div>
+                      </div>
+                  </div>
+            </div>
+        </div>
+      </div>
+      <div class="container" style="margin-top: 10%">
         <div class="row">
             <div class="col">
                 <h1 class="text-center mt-3">Layanan Kami</h1>
@@ -70,7 +153,7 @@ if (!isset($_SESSION['username'])) {
             <div class="col">
                 <div class="row">
                     <div class="col-sm-4">
-                      <div class="card shadow mb-3">
+                      <div class="card shadow mb-3 " style="width: 22rem; height: 25rem;">
                         <div class="card-body">
                           <h4 class="card-title">Penyewaan Mobil</h4>
                           <hr>
@@ -79,7 +162,7 @@ if (!isset($_SESSION['username'])) {
                       </div>
                     </div>
                     <div class="col-sm-4">
-                      <div class="card shadow mb-3">
+                      <div class="card shadow mb-3"  style="width: 22rem; height: 25rem;">
                         <div class="card-body">
                           <h4 class="card-title">Jasa Sopir</h4>
                           <hr>
@@ -88,7 +171,7 @@ if (!isset($_SESSION['username'])) {
                       </div>
                     </div>
                     <div class="col-sm-4">
-                        <div class="card shadow mb-3">
+                        <div class="card shadow mb-3" style="width: 22rem; height: 25rem;">
                           <div class="card-body">
                             <h4 class="card-title">Pelaporan & Pengembalian</h4>
                             <hr>
@@ -101,11 +184,95 @@ if (!isset($_SESSION['username'])) {
         </div>
       </div>
       <div class="container mt-5">
-        <div class="row">
-            <div class="col">
-                <h1>Kontak Kami</h1>
-            </div>
-        </div>
+      <div class="container" style="margin-bottom: 5rem;">
+			<div class="row justify-content-center mb-5">
+				<h1 class="text-center mb-3">Kontak Kami</h1>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-lg-10 col-md-12">
+					<div class="wrapper">
+						<div class="row no-gutters">
+							<div class="col-md-7 d-flex align-items-stretch">
+								<div class="contact-wrap w-100 p-md-5 p-4">
+									<h3 class="mb-4">Get in touch</h3>
+									<div id="form-message-warning" class="mb-4"></div> 
+				      		<div id="form-message-success" class="mb-4">
+				            Your message was sent, thank you!
+				      		</div>
+									<form method="POST" id="contactForm" name="contactForm">
+										<div class="row">
+											<div class="col-md-6">
+												<div class="form-group">
+													<input type="text" class="form-control" name="name" id="name" placeholder="Name">
+												</div>
+											</div>
+											<div class="col-md-6"> 
+												<div class="form-group">
+													<input type="email" class="form-control" name="email" id="email" placeholder="Email">
+												</div>
+											</div>
+											<div class="col-md-12">
+												<div class="form-group">
+													<input type="text" class="form-control" name="subject" id="subject" placeholder="Subject">
+												</div>
+											</div>
+											<div class="col-md-12">
+												<div class="form-group">
+													<textarea name="message" class="form-control" id="message" cols="30" rows="7" placeholder="Message"></textarea>
+												</div>
+											</div>
+											<div class="col-md-12">
+												<div class="form-group">
+													<input type="submit" value="Send Message" class="btn btn-primary">
+													<div class="submitting"></div>
+												</div>
+											</div>
+										</div>
+									</form>
+								</div>
+							</div>
+							<div class="col-md-5 d-flex align-items-stretch">
+								<div class="info-wrap bg-primary w-100 p-lg-5 p-4">
+									<h3 class="mb-4 mt-md-4">Contact us</h3>
+				        	<div class="dbox w-100 d-flex align-items-start">
+				        		<div class="icon d-flex align-items-center justify-content-center">
+				        			<span class="fa fa-map-marker"></span>
+				        		</div>
+				        		<div class="text pl-3">
+					            <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
+					          </div>
+				          </div>
+				        	<div class="dbox w-100 d-flex align-items-center">
+				        		<div class="icon d-flex align-items-center justify-content-center">
+				        			<span class="fa fa-phone"></span>
+				        		</div>
+				        		<div class="text pl-3">
+					            <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
+					          </div>
+				          </div>
+				        	<div class="dbox w-100 d-flex align-items-center">
+				        		<div class="icon d-flex align-items-center justify-content-center">
+				        			<span class="fa fa-paper-plane"></span>
+				        		</div>
+				        		<div class="text pl-3">
+					            <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
+					          </div>
+				          </div>
+				        	<div class="dbox w-100 d-flex align-items-center">
+				        		<div class="icon d-flex align-items-center justify-content-center">
+				        			<span class="fa fa-globe"></span>
+				        		</div>
+				        		<div class="text pl-3">
+					            <p><span>Website</span> <a href="#">yoursite.com</a></p>
+					          </div>
+				          </div>
+			          </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
       </div> 
       
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
--- public/index.php
+++ public/index.php
@@ -44,16 +44,17 @@ $conn->close();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <div class="container mt-5">
     <div class="col-md-6 mx-auto">
-        <div class="card">
+        <div class="card card-login">
             <div class="card-body">
                 <h1 class="text-center">Login</h1>
                 
-                <form method="POST" action="login.php">
+                <form method="POST" action="index.php">
                     <div class="mb-3">
                         <label for="username" class="form-label">Username</label>
                         <input type="text" name="username" class="form-control" id="username" required>
